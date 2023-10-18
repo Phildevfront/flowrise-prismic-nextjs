203b8ffdebd0b9696f4f708140ecd2212b3b83fd
p.baurens <p.baurens@mazarine.com>
2023-10-18 15:22:35 +0200

    Feat: Header module style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
 import Bounded from "@/components/Bounded";
 import Logo from "@/components/Logo"
-
+import styles from "./Header.module.scss"
 
 export default async function Header() {
 
@@ -12,16 +12,16 @@ export default async function Header() {
 	const settings = await client.getSingle("settings")
 
 	return (
-	<Bounded as="header" className="py-4 md:py-6 lg:py-8">
-		<div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
+	<Bounded as="header" className={styles.header}>
+		<div className={styles.header_container}>
 			<Link href="/">
 				<Logo />
 			</Link>
 			<nav>
-				<ul className="flex">
+				<ul className={styles.nav_menu}> 
 					{settings.data.navigation.map(({link, label }) => (
 						<li key={label}>
-							<PrismicNextLink field={link} className="p-3 text-slate-600 font-display font-bold">{label}</PrismicNextLink>
+							<PrismicNextLink field={link} className={styles.nav_menu_link}>{label}</PrismicNextLink>
 						</li>
 					))}
 				</ul>
@@ -31,3 +31,12 @@ export default async function Header() {
 	);
 }
 
+// header className="py-4 md:py-6 lg:py-8"
+
+//  className="flex gap-4 items-center justify-between sm:flex-row flex-col"
+
+//  nav_menu className="flex"> display: flex
+
+// nav_menu_link li className="p-3 text-slate-600 font-display font-bold">
+
+//
